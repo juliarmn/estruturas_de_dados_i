30859a41373ab7fb4d208055f074c30605a9cc38
juliarmn <julia.rm.nascimento@unesp.br>
2023-04-23 15:50:08 -0300

    Arrumando a árvore

--- aula_3/static tree/tree.h
+++ aula_3/static tree/tree.h
@@ -1,11 +1,13 @@
 #ifndef TREE_H
 #define TREE_H
 #include <stdbool.h>
+typedef struct node T_node;
+typedef struct tree T_tree;
 void init_tree(T_tree *tree);
 void create_root(T_tree* tree, int value);
 int get_empty_node (T_tree *tree);
-bool insert_left_child (T_tree *tree, T_node *parent, int value);
-bool insert_right_child (T_tree *tree, T_node *parent, int value);
+void insert_left_child (T_tree *tree, T_node *parent, int value);
+void insert_right_child (T_tree *tree, T_node *parent, int value);
 void create_root (T_tree* tree, int value);
 int calc_height (T_tree *tree, int index);
 int calc_number_of_nodes (T_tree* tree);
